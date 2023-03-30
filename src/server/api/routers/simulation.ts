57a1c11ee2cee69da980192c53d7376aacc507d0
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-03-30 13:39:51 -0500

    Update to assign seats

--- src/server/api/routers/simulation.ts
+++ src/server/api/routers/simulation.ts
@@ -1,7 +1,173 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { z } from "zod";
+import type { passenger as Passenger, seat as Seat } from "@prisma/client";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import keysFromSnakeToCamel from "~/utils/keysFromSnakeToCamel";
+import { keysFromSnakeToCamel } from "~/utils/keysFromSnakeToCamel";
+import { groupBy } from "~/utils/groupBy";
+import { capitalLetterToNumber } from "~/utils/capitalLetterToNumber";
+
+interface PassengersWithBoardingPass extends Passenger {
+  boarding_pass_id: number;
+  purchase_id: number;
+  seat_type_id: number;
+  seat_id: number | null;
+}
+
+interface AssignSeatsArgs {
+  passengers: PassengersWithBoardingPass[];
+  seats: Seat[];
+}
+
+interface GroupPassengersByPurchaseAndSortByAgeArgs {
+  passengers: PassengersWithBoardingPass[];
+}
+
+interface AssignSeatsForGroupArgs {
+  group: PassengersWithBoardingPass[];
+  airplaneSeats: Seat[];
+}
+
+interface FindAdjacentSeatsArgs {
+  group: PassengersWithBoardingPass[];
+  airplaneSeats: Seat[];
+}
+
+interface AssignSeatsForMinorsArgs {
+  group: PassengersWithBoardingPass[];
+  airplaneSeats: Seat[];
+}
+
+interface AssignRemainingSeatsArgs {
+  group: PassengersWithBoardingPass[];
+  airplaneSeats: Seat[];
+}
+
+function assignSeats({ passengers, seats }: AssignSeatsArgs) {
+  const passengerGroups = groupPassengersByPurchaseAndSortByAge({ passengers });
+
+  for (const group of passengerGroups) {
+    assignSeatsForGroup({ group, airplaneSeats: seats });
+  }
+}
+
+function groupPassengersByPurchaseAndSortByAge({
+  passengers,
+}: GroupPassengersByPurchaseAndSortByAgeArgs) {
+  const groupedByPurchase = groupBy(passengers, "purchase_id");
+
+  return Object.values(groupedByPurchase).map((group) =>
+    group.sort((a, b) => a.age - b.age)
+  );
+}
+
+function assignSeatsForGroup({
+  group,
+  airplaneSeats,
+}: AssignSeatsForGroupArgs) {
+  const adjacentSeatsIds = findAdjacentSeats({ group, airplaneSeats });
+
+  if (adjacentSeatsIds) {
+    // Assign adjacent seats to passengers
+    for (let i = 0; i < group.length; i++) {
+      const currentPassenger = group[i];
+      const currentAdjacentSeatId = adjacentSeatsIds[i];
+      if (!currentPassenger || !currentAdjacentSeatId) continue;
+      currentPassenger.seat_id = currentAdjacentSeatId;
+    }
+  } else {
+    assignSeatsForMinors({ group, airplaneSeats });
+
+    assignRemainingSeats({ group, airplaneSeats });
+  }
+}
+
+function findAdjacentSeats({ group, airplaneSeats }: FindAdjacentSeatsArgs) {
+  const groupSize = group.length;
+
+  for (const seat of airplaneSeats) {
+    const row = seat.seat_row;
+    const column = capitalLetterToNumber(seat.seat_column);
+
+    const adjacentSeatIds = [];
+    let seatFound = true;
+
+    for (let i = 0; i < groupSize; i++) {
+      const adjacentSeat = airplaneSeats.find(
+        (seat) =>
+          seat.seat_row === row &&
+          capitalLetterToNumber(seat.seat_column) === column + i
+      );
+
+      if (adjacentSeat) {
+        adjacentSeatIds.push(adjacentSeat.seat_id);
+      } else {
+        seatFound = false;
+        break;
+      }
+    }
+
+    if (seatFound) {
+      return adjacentSeatIds;
+    }
+  }
+
+  return null;
+}
+
+function assignSeatsForMinors({
+  group,
+  airplaneSeats,
+}: AssignSeatsForMinorsArgs) {
+  let passengersGroup = [...group];
+  const minors = passengersGroup.filter((passenger) => passenger.age < 18);
+
+  for (const minor of minors) {
+    const adult = passengersGroup.find((passenger) => passenger.age >= 18);
+
+    if (adult && adult.seat_id) {
+      const adultSeat = airplaneSeats.find(
+        (seat) => seat.seat_id === adult.seat_id
+      );
+
+      if (adultSeat) {
+        const adultRow = adultSeat.seat_row;
+        const adultColumn = capitalLetterToNumber(adultSeat.seat_column);
+
+        const availableSeatForMinor = airplaneSeats.find(
+          (seat) =>
+            seat.seat_row === adultRow &&
+            (capitalLetterToNumber(seat.seat_column) === adultColumn - 1 ||
+              capitalLetterToNumber(seat.seat_column) === adultColumn + 1)
+        );
+
+        if (availableSeatForMinor) {
+          minor.seat_id = availableSeatForMinor.seat_id;
+        }
+      }
+
+      // Remove the assigned adult and minor from the passengersGroup
+      passengersGroup = passengersGroup.filter(
+        (passenger) => passenger !== adult && passenger !== minor
+      );
+    }
+  }
+}
+
+function assignRemainingSeats({
+  group,
+  airplaneSeats,
+}: AssignRemainingSeatsArgs) {
+  for (const passenger of group) {
+    const availableSeat = airplaneSeats.find(
+      (seat) => seat.seat_type_id === passenger.seat_type_id && !seat.seat_id
+    );
+
+    if (availableSeat) {
+      passenger.seat_id = availableSeat.seat_id;
+    }
+  }
+}
 
 const simulationOutputSchema = z.object({
   code: z.number(),
@@ -75,26 +241,34 @@ export const simulationRouter = createTRPCRouter({
           },
         });
 
-        const passengerFullData = selectedFlightPassengersIds.map(
-          (passengerId) => {
-            const passengerBasicData = passengers.find(
-              (passenger) => passenger.passenger_id === passengerId
-            )!;
-            const passengerBoardingPassData = boardingPassesByFlightId.find(
-              (boardingPass) => boardingPass.passenger_id === passengerId
-            )!;
-            return {
-              boarding_pass_id: passengerBoardingPassData?.boarding_pass_id,
-              purchase_id: passengerBoardingPassData?.purchase_id,
-              seat_type_id: passengerBoardingPassData?.seat_type_id,
-              seat_id: passengerBoardingPassData?.seat_id,
-              ...passengerBasicData,
-            };
-          }
-        );
+        const passengersWithBoardingPass = passengers.map((passenger) => {
+          const passengerBoardingPassData = boardingPassesByFlightId.find(
+            (boardingPass) =>
+              boardingPass.passenger_id === passenger.passenger_id
+          )!;
+
+          return {
+            boarding_pass_id: passengerBoardingPassData?.boarding_pass_id,
+            purchase_id: passengerBoardingPassData?.purchase_id,
+            seat_type_id: passengerBoardingPassData?.seat_type_id,
+            seat_id: passengerBoardingPassData?.seat_id,
+            ...passenger,
+          };
+        });
+
+        const airplaneSeats = await ctx.prisma.seat.findMany({
+          where: {
+            airplane_id: selectedFlight.airplane_id,
+          },
+        });
+
+        assignSeats({
+          passengers: passengersWithBoardingPass,
+          seats: airplaneSeats,
+        });
 
         const checkInSimulation = keysFromSnakeToCamel({
-          passengers: passengerFullData,
+          passengers: passengersWithBoardingPass,
           ...selectedFlight,
         });
 
