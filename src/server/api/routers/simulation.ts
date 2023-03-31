c1645ee0c24f4332f18e90dedba9c6aed24d13e6
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-03-30 22:14:40 -0500

    Update simulation router

--- src/server/api/routers/simulation.ts
+++ src/server/api/routers/simulation.ts
@@ -26,6 +26,7 @@ interface GroupPassengersByPurchaseAndSortByAgeArgs {
 interface AssignSeatsForGroupArgs {
   group: PassengersWithBoardingPass[];
   airplaneSeats: Seat[];
+  assignedSeatIds: Set<number | null>;
 }
 
 interface FindAdjacentSeatsArgs {
@@ -33,21 +34,22 @@ interface FindAdjacentSeatsArgs {
   airplaneSeats: Seat[];
 }
 
-interface AssignSeatsForMinorsArgs {
-  group: PassengersWithBoardingPass[];
-  airplaneSeats: Seat[];
-}
-
 interface AssignRemainingSeatsArgs {
   group: PassengersWithBoardingPass[];
   airplaneSeats: Seat[];
+  assignedSeatIds: Set<number | null>;
 }
 
 function assignSeats({ passengers, seats }: AssignSeatsArgs) {
   const passengerGroups = groupPassengersByPurchaseAndSortByAge({ passengers });
+  const assignedSeatIds = new Set(
+    passengers
+      .filter((passenger) => passenger.seat_id !== null)
+      .map((passenger) => passenger.seat_id)
+  );
 
   for (const group of passengerGroups) {
-    assignSeatsForGroup({ group, airplaneSeats: seats });
+    assignSeatsForGroup({ group, airplaneSeats: seats, assignedSeatIds });
   }
 }
 
@@ -57,114 +59,191 @@ function groupPassengersByPurchaseAndSortByAge({
   const groupedByPurchase = groupBy(passengers, "purchase_id");
 
   return Object.values(groupedByPurchase).map((group) =>
-    group.sort((a, b) => a.age - b.age)
+    group.sort((a, b) => {
+      if (a.seat_id === null && b.seat_id !== null) return 1;
+      if (a.seat_id !== null && b.seat_id === null) return -1;
+      return 0;
+    })
   );
 }
 
 function assignSeatsForGroup({
   group,
   airplaneSeats,
+  assignedSeatIds,
 }: AssignSeatsForGroupArgs) {
   const adjacentSeatsIds = findAdjacentSeats({ group, airplaneSeats });
 
   if (adjacentSeatsIds) {
-    // Assign adjacent seats to passengers
+    const minors = group.filter((passenger) => passenger.age < 18);
+    const adults = group.filter((passenger) => passenger.age >= 18);
+
+    let minorIndex = 0;
+    let adultIndex = 0;
+
+    // Assign adjacent seats to passengers, prioritizing minors seated next to adults
     for (let i = 0; i < group.length; i++) {
-      const currentPassenger = group[i];
       const currentAdjacentSeatId = adjacentSeatsIds[i];
-      if (!currentPassenger || !currentAdjacentSeatId) continue;
-      currentPassenger.seat_id = currentAdjacentSeatId;
+      const nextAdjacentSeatId = adjacentSeatsIds[i + 1];
+      const currentMinor = minors[minorIndex];
+      const currentAdult = adults[adultIndex];
+
+      if (
+        currentMinor &&
+        currentAdjacentSeatId &&
+        currentAdult &&
+        nextAdjacentSeatId
+      ) {
+        currentMinor.seat_id = currentAdjacentSeatId;
+        assignedSeatIds.add(currentAdjacentSeatId);
+        minorIndex++;
+
+        currentAdult.seat_id = nextAdjacentSeatId;
+        assignedSeatIds.add(nextAdjacentSeatId);
+        adultIndex++;
+
+        i++; // Skip the next seat since we've already assigned it
+      } else if (currentAdult && currentAdjacentSeatId) {
+        currentAdult.seat_id = currentAdjacentSeatId;
+        assignedSeatIds.add(currentAdjacentSeatId);
+        adultIndex++;
+      }
     }
   } else {
-    assignSeatsForMinors({ group, airplaneSeats });
-
-    assignRemainingSeats({ group, airplaneSeats });
+    assignRemainingSeats({ group, airplaneSeats, assignedSeatIds });
   }
 }
 
-function findAdjacentSeats({ group, airplaneSeats }: FindAdjacentSeatsArgs) {
+function findAdjacentSeats({
+  group,
+  airplaneSeats,
+}: FindAdjacentSeatsArgs): number[] | null {
   const groupSize = group.length;
+  const maxGroupSize = 3; // Define the maximum group size for finding adjacent seats
 
-  for (const seat of airplaneSeats) {
-    const row = seat.seat_row;
-    const column = capitalLetterToNumber(seat.seat_column);
-
-    const adjacentSeatIds = [];
-    let seatFound = true;
-
-    for (let i = 0; i < groupSize; i++) {
-      const adjacentSeat = airplaneSeats.find(
-        (seat) =>
-          seat.seat_row === row &&
-          capitalLetterToNumber(seat.seat_column) === column + i
-      );
-
-      if (adjacentSeat) {
-        adjacentSeatIds.push(adjacentSeat.seat_id);
-      } else {
-        seatFound = false;
-        break;
-      }
+  // If the group size is larger than the maxGroupSize, find adjacent seats for subgroups
+  if (groupSize > maxGroupSize) {
+    const subgroups: PassengersWithBoardingPass[][] = [];
+    for (let i = 0; i < groupSize; i += maxGroupSize) {
+      const subgroup = group.slice(i, i + maxGroupSize);
+      subgroups.push(subgroup);
     }
 
-    if (seatFound) {
-      return adjacentSeatIds;
+    const adjacentSeatsIdsForSubgroups = findAdjacentSeatsForSubgroups({
+      subgroups,
+      airplaneSeats,
+    });
+
+    if (adjacentSeatsIdsForSubgroups) {
+      return adjacentSeatsIdsForSubgroups.flat();
     }
+
+    return null;
+  }
+
+  // Find the first passenger in the group with an assigned seat
+  const firstAssignedPassenger = group.find(
+    (passenger) => passenger.seat_id !== null
+  );
+
+  let startingSeat: Seat | undefined;
+  if (firstAssignedPassenger) {
+    startingSeat = airplaneSeats.find(
+      (seat) => seat.seat_id === firstAssignedPassenger.seat_id
+    );
+  }
+
+  if (!startingSeat) {
+    startingSeat = airplaneSeats[0];
+    if (!startingSeat) return null;
+  }
+
+  const row = startingSeat.seat_row;
+  const column = capitalLetterToNumber(startingSeat.seat_column);
+
+  const adjacentSeatIds: number[] = [];
+  let seatsFound = true;
+
+  for (let i = 0; i < groupSize; i++) {
+    const currentPassenger = group[i];
+    const adjacentSeat = airplaneSeats.find(
+      (seat) =>
+        seat.seat_row === row &&
+        capitalLetterToNumber(seat.seat_column) === column + i &&
+        (seat.seat_type_id === currentPassenger?.seat_type_id ||
+          seat.seat_id === currentPassenger?.seat_id)
+    );
+
+    if (adjacentSeat) {
+      adjacentSeatIds.push(adjacentSeat.seat_id);
+    } else {
+      seatsFound = false;
+      break;
+    }
+  }
+
+  if (seatsFound) {
+    return adjacentSeatIds;
   }
 
   return null;
 }
 
-function assignSeatsForMinors({
-  group,
+function findAdjacentSeatsForSubgroups({
+  subgroups,
   airplaneSeats,
-}: AssignSeatsForMinorsArgs) {
-  let passengersGroup = [...group];
-  const minors = passengersGroup.filter((passenger) => passenger.age < 18);
+}: {
+  subgroups: PassengersWithBoardingPass[][];
+  airplaneSeats: Seat[];
+}) {
+  const adjacentSeatsIdsForSubgroups = [];
 
-  for (const minor of minors) {
-    const adult = passengersGroup.find((passenger) => passenger.age >= 18);
+  for (const subgroup of subgroups) {
+    const adjacentSeatsIds = findAdjacentSeats({
+      group: subgroup,
+      airplaneSeats,
+    });
 
-    if (adult && adult.seat_id) {
-      const adultSeat = airplaneSeats.find(
-        (seat) => seat.seat_id === adult.seat_id
-      );
-
-      if (adultSeat) {
-        const adultRow = adultSeat.seat_row;
-        const adultColumn = capitalLetterToNumber(adultSeat.seat_column);
-
-        const availableSeatForMinor = airplaneSeats.find(
-          (seat) =>
-            seat.seat_row === adultRow &&
-            (capitalLetterToNumber(seat.seat_column) === adultColumn - 1 ||
-              capitalLetterToNumber(seat.seat_column) === adultColumn + 1)
-        );
-
-        if (availableSeatForMinor) {
-          minor.seat_id = availableSeatForMinor.seat_id;
-        }
-      }
-
-      // Remove the assigned adult and minor from the passengersGroup
-      passengersGroup = passengersGroup.filter(
-        (passenger) => passenger !== adult && passenger !== minor
-      );
+    if (adjacentSeatsIds) {
+      adjacentSeatsIdsForSubgroups.push(adjacentSeatsIds);
+    } else {
+      return null;
     }
   }
+
+  return adjacentSeatsIdsForSubgroups;
 }
 
 function assignRemainingSeats({
   group,
   airplaneSeats,
+  assignedSeatIds,
 }: AssignRemainingSeatsArgs) {
   for (const passenger of group) {
+    if (passenger.seat_id) continue;
+
     const availableSeat = airplaneSeats.find(
-      (seat) => seat.seat_type_id === passenger.seat_type_id && !seat.seat_id
+      (seat) =>
+        seat.seat_type_id === passenger.seat_type_id &&
+        !assignedSeatIds.has(seat.seat_id)
     );
 
     if (availableSeat) {
       passenger.seat_id = availableSeat.seat_id;
+      assignedSeatIds.add(availableSeat.seat_id);
+    } else {
+      // If no available seat of the same type is found, try to assign a seat of a different type
+      const alternativeSeatType = passenger.seat_type_id === 1 ? 2 : 1;
+      const alternativeSeat = airplaneSeats.find(
+        (seat) =>
+          seat.seat_type_id === alternativeSeatType &&
+          !assignedSeatIds.has(seat.seat_id)
+      );
+
+      if (alternativeSeat) {
+        passenger.seat_id = alternativeSeat.seat_id;
+        assignedSeatIds.add(alternativeSeat.seat_id);
+      }
     }
   }
 }
